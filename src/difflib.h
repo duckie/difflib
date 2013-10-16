d2338e4dc8f5d693fc88e5a9642b02a300df82bc
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2013-10-16 23:53:06 +0200

    Test of a trick.

--- src/difflib.h
+++ src/difflib.h
@@ -6,8 +6,44 @@
 #include <utility>
 #include <iterator>
 #include <type_traits>
+#include <iostream>
 
 namespace difflib {
+
+// Found here : http://stackoverflow.com/questions/11328264/python-like-loop-enumeration-in-c
+template<typename Iterable> class EnumerateObject {
+Iterable _iter;
+size_t _size;
+decltype(_iter.begin()) _begin;
+
+public:
+  template<class Iterable2>
+  EnumerateObject(Iterable2&& iter):
+    _iter(std::forward<Iterable2>(iter)),
+    _size(0),
+    _begin(iter.begin())
+  {}
+
+  const EnumerateObject& begin() const { return *this; }
+  const EnumerateObject& end()   const { return *this; }
+
+  bool operator!=(const EnumerateObject&) const {
+    return _begin != _iter.end();
+  }
+
+  void operator++() {
+    ++_begin;
+    ++_size;
+  }
+
+  auto operator*() const -> std::tuple<size_t const, decltype(*_begin)> {
+    return std::move(std::tuple<size_t const, decltype(*_begin)>(_size, *_begin));
+   }
+};
+
+template<typename Iterable> EnumerateObject<Iterable> enumerate(Iterable&& iter) {
+      return { std::forward<Iterable>(iter) };
+}
 
 // This trait checks if a given type is a standard collection of hashable types
 // SFINAE is nice with C++11 and decltype
@@ -46,7 +82,11 @@ template <class T = std::string> class SequenceMatcher {
   using hashable_type = typename T::value_type;
   using junk_function_type = std::function<bool(hashable_type const&)>;
 
-  SequenceMatcher(T const& a, T const& b, junk_function_type is_junk = NoJunk<hashable_type>() ) : a_(a), b_(b), is_junk_(is_junk) {}
+  SequenceMatcher(T const& a, T const& b, junk_function_type is_junk = NoJunk<hashable_type>()): 
+  a_(a), b_(b), is_junk_(is_junk) {
+    set_seq(a,b);
+  }
+
   SequenceMatcher(SequenceMatcher<T> const&) = delete;
   SequenceMatcher& operator= (SequenceMatcher<T> const&) = delete;
   SequenceMatcher(SequenceMatcher<T>&&) = default;
@@ -63,7 +103,13 @@ template <class T = std::string> class SequenceMatcher {
 
   void set_seq2(T const& b) {
     b_ = b;
-    // TODO
+    size_t i=0;
+    T elem;
+    for(auto const a : enumerate(b)) {
+      std::tie(i, elem) = a;
+      std::cout << i << ":" << elem << "\n";
+    }
+    std::cout << std::flush;
   }
 
 
