de82cf3ff71ed4d5a94ff3c27bc8cdf93cce0d22
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2014-07-21 00:42:45 +0200

    Better random datas

--- test/gendata2.sh
+++ test/gendata2.sh
@@ -4,19 +4,20 @@ size_common=$1
 size_not_common=$2
 nb_alternates=$3
 
-gen_common="cat /dev/urandom | base64 | tr -dc \"[:alnum:]\" | head -c${size_common} | tr -s \"\n\" \" \"|tr -d \" \""
-gen_uncommon="cat /dev/urandom | base64 | tr -dc \"[:alnum:]\" | head -c${size_not_common} | tr -s \"\n\" \" \"|tr -d \" \""
+function gen_random {
+  cat /dev/urandom | base64 | tr -dc "[:alnum:]" | head -c$1 | tr -s "\n" " "|tr -d " "
+}
 
 echo "" > /tmp/a
 echo "" > /tmp/b
 for i in $(seq 0 $nb_alternates); do
   if [[ $((${i}%2)) == 0 ]]; then
-    eval ${gen_common} > /tmp/common
+    gen_random $(($RANDOM%${size_common})) > /tmp/common
     cat /tmp/common >> /tmp/a
     cat /tmp/common >> /tmp/b
   else
-    eval ${gen_uncommon} >> /tmp/a
-    eval ${gen_uncommon} >> /tmp/b
+    gen_random $(($RANDOM%${size_not_common})) >> /tmp/a
+    gen_random $(($RANDOM%${size_not_common})) >> /tmp/b
   fi
 done
 
